ae2b0c165f44aa3862c686f69ec51711d66e1384
HuangStomach <nxmbest@qq.com>
2023-07-21 11:06:18 +0800

    init with spring initializr

---
+++ src/main/java/com/huangstomach/springalchemy/SpringAlchemyApplication.java
@@ -0,0 +1,13 @@
+package com.huangstomach.springalchemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlchemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlchemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huangstomach/springalchemy/SpringAlchemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangstomach.springalchemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlchemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
