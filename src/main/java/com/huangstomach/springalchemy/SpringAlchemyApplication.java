38c78f84435369fb9342da1e50cb8371471b67f8
HuangStomach <nxmbest@qq.com>
2023-07-23 19:47:57 +0800

    async task

--- src/main/java/com/huangstomach/springalchemy/SpringAlchemyApplication.java
+++ src/main/java/com/huangstomach/springalchemy/SpringAlchemyApplication.java
@@ -8,6 +8,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 import com.huangstomach.springalchemy.storage.StorageProperties;
 
@@ -17,18 +20,29 @@ import com.huangstomach.springalchemy.storage.StorageProperties;
 @EnableAsync
 public class SpringAlchemyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAlchemyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAlchemyApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		// 全局CORS配置
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/user").allowedOrigins("http://localhost:8080");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        // 全局CORS配置
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/user").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLookup-");
+        executor.initialize();
+        return executor;
+    }
 }
